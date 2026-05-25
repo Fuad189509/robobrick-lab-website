7ed7625de6d166223e17dfc0e19c4d8e33d24b51
Fuad <fuad@example.com>
2026-05-25 14:25:45 +0700

    Add simple /test-laravel route to check Laravel status without views

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 
+// Simple test route to check if Laravel is running
+Route::get('/test-laravel', function () {
+    return response()->json([
+        'status' => 'ok',
+        'message' => 'Laravel is running!',
+        'laravel_version' => app()->version(),
+        'php_version' => phpversion(),
+    ]);
+});
+
 Route::get('/', [PageController::class, 'home'])->name('home');
 Route::get('/tentang', [PageController::class, 'about'])->name('about');
 Route::get('/program', [PageController::class, 'program'])->name('program');
