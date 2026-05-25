d45bffb78a1507990910f67fc3749e4e9da587ad
Fuad <fuad@example.com>
2026-05-25 13:52:20 +0700

    Fix useStoragePath() call on Application instance instead of ApplicationBuilder

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,14 +4,8 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-$app = Application::configure(basePath: dirname(__DIR__));
-
-// Use custom storage path if defined (for Vercel)
-if (defined('LARAVEL_STORAGE_PATH')) {
-    $app->useStoragePath(LARAVEL_STORAGE_PATH);
-}
-
-return $app
+// Create the application first
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -23,3 +17,10 @@ return $app
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
+// Now set the storage path on the Application instance
+if (defined('LARAVEL_STORAGE_PATH')) {
+    $app->useStoragePath(LARAVEL_STORAGE_PATH);
+}
+
+return $app;
